3856663c7603c601e75a55c2131479d831347e2a
siu98 <101093309+siu98@users.noreply.github.com>
2025-03-01 11:39:44 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,23 +1,26 @@
-#백준2606: 바이러스
 import sys
 input = sys.stdin.readline
-num = int(input())
-network = int(input())
-graph = [[] for _ in range(num+1)]
-for _ in range(network):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
 
-def dfs(x):
+computer = int(input())
+graph = [[] for _ in range(computer+1)]
+network = int(input())
+visited = [False]*(computer+1)
+for _ in range(network):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+
+def DFS(v):
     global count
-    visited[x] = True
+    visited[v] = True
     count += 1
-    for node in graph[x]:
-        if visited[node]:
+    for i in graph[v]:
+        if visited[i]:
             continue
-        dfs(node)
-count = 0
-visited = [False for _ in range(num+1)]
-dfs(1)
+
+        DFS(i)
+
+count = 0        
+DFS(1)
 print(count-1)
