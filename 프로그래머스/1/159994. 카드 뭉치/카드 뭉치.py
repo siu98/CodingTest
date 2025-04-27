07ea8f3ad38ad35378a5a97e14004b92e019d210
siuuuo3o <101093309+siuuuo3o@users.noreply.github.com>
2025-04-27 09:35:34 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,15 @@
+def solution(cards1, cards2, goal):
+    answer = []
+    n = len(cards1)
+    m = len(cards2)
+    
+    i = 0
+    j = 0
+    for word in goal:
+        if i<n and word==cards1[i]:
+            answer.append(cards1[i])
+            i += 1
+        if j<m and word==cards2[j]:
+            answer.append(cards2[j])
+            j += 1
+    return 'Yes' if answer==goal else 'No'
